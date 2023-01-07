74a799975682d20e29571a71ecfbe23050708928
Akbar Nafisa Ja'far <akbar.nafisa@dana.id>
2023-01-07 15:07:41 +0700

    finish challange #3

---
+++ src/ChristmasTree.vue
@@ -0,0 +1,24 @@
+<script setup lang="tsx">
+import { FunctionalComponent } from 'vue'
+
+const props = withDefaults(
+  defineProps<{
+    size: number
+  }>(),
+  {
+    size: 1,
+  }
+)
+// use TSX
+const Ball: FunctionalComponent = () => {
+  return <div class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
+}
+</script>
+
+<template>
+  <div>
+    <div v-for="i in size" class="flex justify-center">
+      <Ball v-for="j in i" />
+    </div>
+  </div>
+</template>
